0eec300ad5c0cd2091445ee01998859f2e9557d5
Sneha-Sukumar <72868301+Sneha-Sukumar@users.noreply.github.com>
2023-09-03 17:54:41 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,28 @@
-class Node
-{
-    int val;
-    Node left,right;
-    Node(int val)
-    {
-        this.val=val;
-        left=right=null;
-    }
-}
-class Main
-{
-    Node node;
-    Node prev = null;
-    Node newHead = null;
-    void processBST(Node root)
-    {
-        if(root == null)return;
-        processBST(root.left);
-        Node rightNode = root.right;
-        Node leftNode = root.left;
-        if(newHead == null)
-        {
-            newHead = root;
-            root.left = null;
-            prev = root;
-        }
-        else
-        {
-            prev.right = root;
-            root.left = null;
-            prev = root;
-        }
-        processBST(rightNode);
-    }
-    void traverse(Node root)
-    {
-        if(root == null)return;
-        System.out.print(root.val + " ");
-        traverse(root.right);
-    }
+import java.util.*;
+public class Main {
     public static void main(String[] args) {
-        Main tree = new Main();
-        tree.node = new Node(50);
-        tree.node.left = new Node(30);
-        tree.node.right = new Node(60);
-        tree.node.left.left = new Node(10);
-        tree.node.right.left= new Node(55);
-        tree.processBST(tree.node);
-        tree.traverse(tree.newHead);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total number floors in the building : ");
+        int n = sc.nextInt();
+        int[] day = new int[n+1];
+        for(int i=1;i<=n;i++) {
+            System.out.println("Enter the floor size given on day : "+i);
+            int size = sc.nextInt();
+            day[size]=i;
+        }
+        int temp = n;
+        int flag;
+        System.out.println("The order of construction is as follows ");
+        for(int i=1;i<=n;i++){
+            flag=0;
+            System.out.println("Day "+i+" :");
+            while( temp >=1 && day[temp] <= i){
+                flag = 1;
+                System.out.print(temp +" ");
+                temp--;
+            }
+            if(flag==1)
+                System.out.println();
+        }
     }
 }
